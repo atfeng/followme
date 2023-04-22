58bb8a062c95e15880b31143f0a720872b1b42db
Athurg Feng <i@atfeng.com>
2023-04-22 18:40:35 +0800

    增加使用CoreData持久化存储轨迹点的功能

--- followme/ContentView.swift
+++ followme/ContentView.swift
@@ -6,8 +6,15 @@ struct ContentView: View {
 
 	var body: some View {
 		VStack{
-			Map(coordinateRegion: $locationManager.mapRegion, showsUserLocation: true, annotationItems: locationManager.coordinates) { item in
-				MapAnnotation(coordinate: item.coordinate) {
+			Map(coordinateRegion: $locationManager.mapRegion,
+				showsUserLocation: true,
+				annotationItems: locationManager.coordinates
+			) { item in
+				MapAnnotation(
+					coordinate: CLLocationCoordinate2D(latitude: item.latitude,
+													   longitude: item.longitude
+													  )
+				) {
 					Circle().foregroundColor(.red)
 				}
 			}.edgesIgnoringSafeArea(.all)
--- followme/LocationManager.swift
+++ followme/LocationManager.swift
@@ -1,20 +1,29 @@
 import MapKit
 import CoreLocation
-
-//IdentifiableCoordinate 用于将坐标显示到地图上（需要遵循Identifiable协议）
-struct IdentifiableCoordinate: Identifiable {
-	var id = UUID()
-	var coordinate: CLLocationCoordinate2D
-}
+import CoreData
 
 //LocationManager用于定位相关的功能，作为CLLocationManager的delegate对象
 class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject  {
 	@Published var mapRegion = MKCoordinateRegion()
-	@Published var coordinates: [IdentifiableCoordinate] = []
+	@Published var coordinates: [Coordinate] = []
 	private var locationManager = CLLocationManager()
+	lazy var persistentContainer: NSPersistentContainer = {
+		let container = NSPersistentContainer(name: "Followme")
+		container.loadPersistentStores { description, error in
+			if let error = error {
+				fatalError("Unable to load persistent stores: \(error)")
+			}
+		}
+		return container
+	}()
 
 	override init() {
 		super.init()
+		let fetchRequest = Coordinate.fetchRequest()
+		let historyCoordinates = try? persistentContainer.viewContext.fetch(fetchRequest)
+		if let historyCoordinates {
+			self.coordinates.append(contentsOf: historyCoordinates)
+		}
 		locationManager.delegate = self
 		locationManager.requestAlwaysAuthorization()
 		locationManager.startUpdatingLocation()
@@ -32,7 +41,14 @@ class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject  {
 			longitudinalMeters: 1000
 		)
 
-		let newCoordinates = locations.map{ IdentifiableCoordinate(coordinate: $0.coordinate) }
-		self.coordinates.append(contentsOf: newCoordinates)
+		let coordinateEntity = Coordinate.entity()
+		for location in locations {
+			let coordinate = Coordinate(entity: coordinateEntity, insertInto: persistentContainer.viewContext)
+			coordinate.time = location.timestamp
+			coordinate.latitude = location.coordinate.latitude
+			coordinate.longitude = location.coordinate.longitude
+			self.coordinates.append(coordinate)
+		}
+		try? persistentContainer.viewContext.save()
 	}
 }
